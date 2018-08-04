33d3a1b1b8fb2182d40d66cb6f23f883a58e02d3
meng-Shiun <lin.mengshiun@gmail.com>
2018-08-04 11:11:44 +0200

    feat: Make Youtube API call via youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyCgijOAYHAubfTkf2sveenUh_wpFyB2B2A'
+
+YTSearch({key: API_KEY, term: 'ice cream' }, function(data) {
+  console.log(data)
+})
 
 const App = () => {
   return (
